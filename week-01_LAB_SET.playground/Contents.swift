b82ceec13de400ff5a2ce31db735013fdb8d1e52
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-07 14:27:59 +0300

    LAB_SET

---
+++ week-01_LAB_SET.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var ahmed : Set = ["Hail", "Riyadh", "Dubai"]
+var faris : Set = ["Riyadh", "Jizan", "Abu Dhabi", "Hail"]
+
+
+var ahmedVisited = ahmed.union(faris)
+var farisVisited = ahmed.intersection(faris)
+var farisNotVisited = ahmed.subtracting(faris)
+print("Union:  ")
+for visited in ahmedVisited{
+    print("\(visited)")
+          
+          }
+
+print("intersection: ")
+
+for visited1 in farisVisited{
+    print( "\(visited1)")
+          
+          }
+print("subtraction: ")
+
+for unVisited in farisNotVisited{
+    
+    print("The cities Faris not visited: \(unVisited)")
+}
