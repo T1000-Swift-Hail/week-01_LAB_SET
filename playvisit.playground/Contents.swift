c1c571b9bb30e8ebda0e8191756b678ee07f2977
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-07 15:02:11 +0300

    Finish

--- playvisit.playground/Contents.swift
+++ playvisit.playground/Contents.swift
@@ -5,17 +5,18 @@ var Farisvisite  : Set<String> = ["Riyadh", "Jizan", "Abu Dhabi", "Hail"]
 
 var newvisite = Ahmadvisite.union(Farisvisite)
 var newvisite1 = Ahmadvisite.intersection(Farisvisite)
-var newvisite2 = Ahmadvisite.subtracting(Farisvisite)
+var newvisite2 = Farisvisite.subtracting(Ahmadvisite)
 
-
+print ( "all visited ")
 for x in newvisite {
     
     print (x)
-    
 }
+
+
         for y in newvisite1 {
-            print (y) }
+            print ("faris and ahmad visit \(y)") }
 
 for z in newvisite2 {
-    print (z)
+    print ("ahmed did not visit \(z) ")
 }
