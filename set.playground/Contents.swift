bbd93ad44353722d1e463f7db65f32d76fdc549f
user <user@MacBook-Air-.local>
2021-10-07 14:27:29 +0300

    Lab Set Is Done (Edit)

--- set.playground/Contents.swift
+++ set.playground/Contents.swift
@@ -5,15 +5,19 @@ var FarisVisited: Set = ["Riyadh", "Jizan", "AbuDhabi", "Hail"]
 
 var AllVisited = AhmadVisited.union(FarisVisited)
 var BoothVisited = AhmadVisited.intersection(FarisVisited)
-var Recommend = AhmadVisited.subtracting(FarisVisited)
+var Recommend = FarisVisited.subtracting(AhmadVisited)
+
+print("All cities:")
 for cities in AllVisited {
     print(cities)
 }
-
+print()
+print("Cities they visited:")
 for theyVisited in BoothVisited {
     print(theyVisited)
 }
-
+print()
+print("Cities Faris visited:")
 for FarisVisit in Recommend {
     print(FarisVisit)
 }
